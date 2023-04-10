f917e46ccb07520c2a4cfa5ffdf7720590ef9a4d
Jess M Johnson <hello@jessmjohnson.com>
2023-04-10 15:11:50 -0400

    add write functions for local postgres, aws s3

--- sandbox-historical.py
+++ sandbox-historical.py
@@ -1,25 +1,73 @@
 from wyze_sdk import Client
 from wyze_sdk.errors import WyzeApiError
 from datetime import datetime, timezone
+
+import boto3
+import pandas as pd
 import os
 import psycopg2
 
+# Wyze credentials
 email = os.environ.get('WYZE_USER')
 password = os.environ.get('WYZE_PSWD')
 
-try:
-    # Connect to the PostgreSQL database
-    conn = psycopg2.connect(
-        database="myhome_iot",
-        user='ETL_USER',
-        password='[ETL PASSWORD]', 
-        host="localhost"
-    )
 
-    # Create a cursor to execute SQL queries
-    cur = conn.cursor()
-except psycopg2.Error as e:
-    print("Error: Could not make connection to the Postgres database")
+def write_to_postgresql(df_results):
+
+    # Postgres Credentials
+    postgres_user = os.environ.get('ETL_USER')
+    postgres_pwd = os.environ.get('ETL_PASSWORD')
+
+    try:
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(
+            database="myhome_iot",
+            user=postgres_user,
+            password=postgres_pwd, 
+            host="localhost"
+        )
+
+        # Create a cursor to execute SQL queries
+        cur = conn.cursor()
+
+    except psycopg2.Error as e:
+        print("Error: Could not make connection to the Postgres database")
+        
+    try:
+        # Insert the temperature data into the table
+        for index, row in df_results.iterrows():
+            cur.execute("INSERT INTO wyze_temperature (sensor_name, device_id, mac_address, product_model, temperature, humidity, battery_level, is_online, create_dt) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (row['sensor_name'], row['device_id'], row['mac_address'], row['product_model'], row['temperature'], row['humidity'], row['battery_level'], row['is_online'], row['create_dt']))
+
+    except psycopg2.Error as e:
+        print(f"Error: Could not insert record into temperature table: {e}")
+    
+    #  Commit the changes to the database and close the connection
+    cur.close()
+    conn.commit()
+    conn.close()
+
+    return 0;
+
+def write_to_s3_bucket(df_results):
+    
+    # AWS credentials
+    AWS_ACCESS_KEY_ID = ''
+    AWS_SECRET_ACCESS_KEY = ''
+    S3_BUCKET_NAME = ''
+
+    try:
+        # Upload the temperature data to S3
+        s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        s3.put_object(Bucket=S3_BUCKET_NAME, Key='temperature_data.txt', Body=df_results)
+
+    except e:
+        print(f"Error: Count not write to AWS S3 Bucket: {e}")
+
+    return 0;
+
+def write_to_azure_blob(df_results):
+    return 0;
+
 
 try: 
     # authentication
@@ -36,8 +84,10 @@ try:
 except WyzeApiError as e:
     # You will get a WyzeApiError if the request failed
     print(f"Got an error: {e}")
-    conn.close() # Close communication with the database
     exit() # Exit the program
+
+# Create a Pandas dataframe from temperature data
+temperature_df = pd.DataFrame(columns=['sensor_name', 'device_id', 'mac_address', 'product_model', 'temperature', 'humidity', 'battery_level', 'is_online', 'create_dt'])
 
 # Loop through the temperature sensors and insert readings into the database
 for r in room_sensors:
@@ -52,93 +102,26 @@ for r in room_sensors:
         is_online = r.is_online # Get the online status
         battery_level = int(r.battery.name.split('_')[1]) # Extract the integer value of the battery level
 
+        temperature_df = temperature_df.append(
+            {
+                'sensor_name': sensor_name, 
+                'device_id': device_id,
+                'mac_address': mac_address,
+                'product_model': product_model,
+                'temperature': temperature,
+                'humidity': humidity,
+                'battery_level': battery_level,
+                'is_online': is_online,
+                'create_dt': create_dt
+            }
+            , ignore_index=True
+        )
+
     except WyzeApiError as e:
         print(e) # Print the error
-        conn.close() # Close communication with the database
         exit() # Exit the program
-    
-    try:
-        cur.execute("INSERT INTO wyze_temperature (sensor_name, device_id, mac_address, product_model, temperature, humidity, battery_level, is_online, create_dt) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (sensor_name, device_id, mac_address, product_model, temperature, humidity, battery_level, is_online, create_dt))
-    except psycopg2.Error as e:
-        print(f"Error: Could not insert record into temperature table: {e}")
 
-# Commit the changes to the database and close the connection
-cur.close()
-conn.commit()
-conn.close()
+print(temperature_df)
 
-"""
-# Create a Wyze client with your access token
-wyze_client = Client(token=wyze_access_token)
-
-# Get a list of devices and filter for temperature sensors
-try:
-    devices = wyze_client.devices_list()
-    sensors = [d for d in devices if d is not None and d.product.model == 'WS01C' and d.product.definition is not None]
-except WyzeApiError as e:
-    print(e)
-    conn.close()
-    exit()
-
-# Loop through the temperature sensors and insert readings into the database
-for sensor in sensors:
-    try:
-        temperature = sensor.sensor.temperature.get_reading().value
-        humidity = sensor.sensor.humidity.get_reading().value
-        cur.execute("INSERT INTO temperature_readings (device_id, temperature, humidity) VALUES (%s, %s, %s)", (sensor.mac, temperature, humidity))
-    except WyzeApiError as e:
-        print(e)
-
-# Commit the changes to the database and close the connection
-conn.commit()
-conn.close()
-
-"""
-"""
-if my_thermostat:
-    # Get the list of room sensors associated with the thermostat
-    room_sensors = client.thermostats.get_sensors(device_mac=my_thermostat.mac, device_model=my_thermostat.product.model)
-
-    # Set the date range for the data you want to export
-    start_time = datetime.datetime(2022, 3, 1, 0, 0, 0)
-    end_time = datetime.datetime(2022, 4, 1, 0, 0, 0)
-
-    # Retrieve the temperature and humidity data for the date range for each room sensor
-    for room_sensor in room_sensors:
-        #temperature_data = client.get_device_history(room_sensor.mac, start_time, end_time, room_sensor.DeviceEventType.TEMPERATURE)
-        #humidity_data = client.get_device_history(room_sensor.mac, start_time, end_time, room_sensor.DeviceEventType.HUMIDITY)
-        
-        print(client.events.get_events(start_time, end_time, room_sensor.mac))
-        
-        # Print out the temperature and humidity readings for the room sensor
-        #for reading in temperature_data:
-        #    print(f'Room Sensor: {room_sensor.nickname}, Temperature: {reading.value}°F, Timestamp: {reading.created_at}')
-        #for reading in humidity_data:
-        #    print(f'Room Sensor: {room_sensor.nickname}, Humidity: {reading.value}%, Timestamp: {reading.created_at}')
-else:
-    print('No Wyze thermostats found.')
-"""
-
-"""
-# If we found a thermostat, retrieve the historical data for the associated room sensor(s)
-if thermostat:
-    # Get the list of room sensors associated with the thermostat
-    room_sensors = client.thermostats.get_sensors(device_mac='CO_EA1_52324837043835324e325cac', device_model='CO_EA1')
-
-    # Set the date range for the data you want to export
-    start_time = datetime.datetime(2022, 3, 1, 0, 0, 0)
-    end_time = datetime.datetime(2022, 4, 1, 0, 0, 0)
-
-    # Retrieve the temperature and humidity data for the date range for each room sensor
-    for room_sensor in room_sensors:
-        temperature_data = client.get_device_history(room_sensor.mac, start_time, end_time, room_sensor.DeviceEventType.TEMPERATURE)
-        humidity_data = client.get_device_history(room_sensor.mac, start_time, end_time, room_sensor.DeviceEventType.HUMIDITY)
-
-        # Print out the temperature and humidity readings for the room sensor
-        for reading in temperature_data:
-            print(f'Room Sensor: {room_sensor.nickname}, Temperature: {reading.value}°F, Timestamp: {reading.created_at}')
-        for reading in humidity_data:
-            print(f'Room Sensor: {room_sensor.nickname}, Humidity: {reading.value}%, Timestamp: {reading.created_at}')
-else:
-    print('No Wyze thermostats found.')
- """
+# Logout from the Wyze Client
+#client.logout()

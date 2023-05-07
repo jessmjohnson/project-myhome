cbdd15783a2cad5bfeb3edb4f5a574e877af4fba
Jess M Johnson <hello@jessmjohnson.com>
2023-05-07 11:49:27 -0400

    integration of kafka, wyze issue investigation

--- home_iot_wyze_temp_kafka.py
+++ home_iot_wyze_temp_kafka.py
@@ -1,91 +1,128 @@
 import os
-import time
+import logging
 from datetime import datetime, timezone
 from kafka import KafkaProducer
 from wyze_sdk import Client
 from wyze_sdk.errors import WyzeApiError
 
 
-"""
-    This function authenticates to the Wyze API.
-"""
-def wyze_authentication(wyze_email, wyze_password):
-    try: 
-        # authentication
-        wyze_response = Client().login(email=wyze_email, password=wyze_password)   # Create a new client and Login to the client 
-        wyze_access_token = wyze_response['access_token'] # Get the access token
-        client = Client(token=wyze_access_token)
+def setup_logging(level: int = logging.INFO) -> None:
+    """Set up logging configuration"""
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=level
+    )
 
-        return client # Return the Wyze API client
 
-    except WyzeApiError as e:
-        # You will get a WyzeApiError if the request failed
-        print(f"Got an error: {e}")
-        exit() # Exit the program
+def create_kafka_producer(bootstrap_server_address: str) -> KafkaProducer:
+    """Set up and return a KafkaProducer instance using the given bootstrap servers address.
 
-"""
-    This function gets the temperature data from the Wyze API.
-"""
-def get_wyze_temperatures(wyze_client, kafka_producer, kafka_topic):
+    Args:
+        bootstrap_server_address (str): The Kafka bootstrap servers address.
 
-    # Get the list of all Wyze devices associated with your account
+    Returns:
+        KafkaProducer: A KafkaProducer instance.
+    """
     try:
-        # Get the list of all Wyze devices associated with your account
-        my_thermostat = wyze_client.thermostats.list()[0]  #get_user_devices()
+        producer = KafkaProducer(bootstrap_servers=bootstrap_server_address)
+    except Exception as e:
+        logging.error(f'Failed to create Kafka producer: {e}')
+        raise
 
-        # Get a list of all Room Sensors for Thermostat devices
-        room_sensors = wyze_client.thermostats.get_sensors(device_mac=my_thermostat.mac, device_model='CO_EA1')
+    return producer
 
-        # Loop through the temperature sensors and insert readings into the database
-        for r in room_sensors:
-                try:
-                    sensor_name = r.nickname # Get the sensor name
-                    device_id = r.did # Get the device ID
-                    mac_address = r.mac # Get the MAC address
-                    product_model = 'WS01C' if r.product.model is None else r.product.model # Get the product model
-                    temperature = r.temperature # Get the temperature
-                except WyzeApiError as e:
-                    print('Failed to get temperature data:', e)
-                    exit(1) # Exit the program
-                    #continue
 
-                try:
-                    # Publish the sensor mac address, temperature, and timestamp data to Kafka
-                    kafka_producer.send(kafka_topic, str([mac_address, temperature, datetime.now(timezone.utc)]).encode('utf-8'))
-                except Exception as e:
-                    print("Error when publishing to kafka: " + str(e))
-                    #logger.error("Error when connecting with kafka consumer: " + str(e))
-                    exit() # Exit the program
+def authenticate_wyze_api(email: str, password: str) -> Client:
+    """
+    Authenticate to the Wyze API and return a client object.
 
+    Args:
+        email (str): Wyze account email.
+        password (str): Wyze account password.
+
+    Returns:
+        Client: Wyze API client object.
+    """
+    try:
+        client = Client().login(email=email, password=password)
+        return client
     except WyzeApiError as e:
-        print(e) # Print the error
-        exit() # Exit the program
+        logging.error(f"Wyze API authentication failed with error: {e}")
+        raise
 
 
-#def main(storage_option, zip_code):
+def get_wyze_temperatures(wyze_client: Client, kafka_producer: KafkaProducer, topic: str) -> None:
+    """
+    Get the temperature data from the Wyze API and publish it to Kafka.
 
-# Wyze credentials
-email = os.environ.get('WYZE_USER')
-password = os.environ.get('WYZE_PSWD')
-kafka_server_address = os.environ.get('KAFKA_SERVER_ADDRESS')
+    Args:
+        wyze_client (Client): Wyze API client object.
+        kafka_producer (KafkaProducer): Kafka producer object.
+        topic (str): Kafka topic to publish the data to.
+    """
+    try:
+        my_thermostat = None
+        for device in wyze_client.devices_list:
+            if device.product_type == 'THERMOSTAT':
+                my_thermostat = device
+                break
 
-# Get the Wyze API client
-wyze_client = wyze_authentication(email, password)
+        if my_thermostat is None:
+            logging.error('No thermostat found in Wyze account')
+            raise
 
-# Create a Kafka producer
-try:
-    producer = KafkaProducer(bootstrap_servers=kafka_server_address)
-except Exception as e:
-    print('Failed to create Kafka producer:', e)
-    exit(1)
+        #thermostat = wyze_client.thermostats.list()[0]
+        room_sensors = wyze_client.thermostats.get_sensors(device_mac=my_thermostat.mac, device_model='CO_EA1')
+        for r in room_sensors:
+            try:
+                sensor_name = r.nickname
+                device_id = r.did
+                mac_address = r.mac
+                product_model = 'WS01C' if r.product.model is None else r.product.model
+                temperature = r.temperature
+                humidity = r.humidity if r.humidity is not None else 0
+                timestamp = datetime.now(timezone.utc)
+            except WyzeApiError as e:
+                logging.error(f"Failed to get temperature data for {r.nickname} with error: {e}")
+                continue
 
-# Define the topic to publish to
-topic = 'wyze-room-sensor-temp'
+            try:
+                message = [mac_address, temperature, humidity, timestamp]
+                kafka_producer.send(topic, str(message).encode('utf-8'))
+            except Exception as e:
+                logging.error(f"Failed to publish to Kafka with error: {e}")
+                raise
+    except WyzeApiError as e:
+        logging.error(f"Failed to get Wyze temperature data with error: {e}")
+        raise
 
-# Loop to collect and publish data every 30 minutes
-while True:
-    # Get the temperature data from the Wyze Room Sensor
-    temperature = get_wyze_temperatures(wyze_client, producer, topic)
+
+def main() -> None:
+    # logging setup
+    setup_logging()
+
+    # get credentials
+    email = os.environ.get('WYZE_USER')
+    password = os.environ.get('WYZE_PSWD')
+    kafka_server_address = os.environ.get('KAFKA_SERVER_ADDRESS')
+
+    # create Wyze client object
+    try:
+        wyze_client = authenticate_wyze_api(email, password)
+    except:
+        logging.error("Wyze API authentication failed")
+        raise
     
-    # Wait for 15 minutes before collecting data again
-    time.sleep(15 * 60)
+    # create Kafka producer object
+    producer = create_kafka_producer(kafka_server_address)
+    
+    # get temperature data and publish to Kafka
+    try:
+        get_wyze_temperatures(wyze_client, producer, 'wyze-room-sensor-temp')
+    except:
+        logging.error("Failed to get and publish temperature data")
+        raise
+
+if __name__ == '__main__':
+    main()
